7d2d7ee332e197f0fe60e40b5948334fcef8d21d
Saulius Vikerta <saulius.vikerta@gmail.com>
2017-01-23 23:59:52 +0200

    Webpack setup

---
+++ app/entries/main.js
@@ -0,0 +1,4 @@
+//import "./styles/main.scss";
+document.write("Bundling works.");
+document.write("Yes");
+console.info("Hello, webpack");
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     context: __dirname,
@@ -32,5 +33,11 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "My Shop",
+            template: "../app/templates/index.html"
+        })
+    ],
     stats: { colors: true }
 };
