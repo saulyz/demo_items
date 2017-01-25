9f9cc23eadaa722472d845e0a35ec45afa752835
Saulius Vikerta <saulius.vikerta@gmail.com>
2017-01-26 00:36:55 +0200

    Changes in server - solving CORS problem

--- server/index.js
+++ server/index.js
@@ -3,6 +3,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain);
+
 app.use('/items', require('./routes/itemsRouter'));
 app.use('/item', require('./routes/itemRouter'));
 
