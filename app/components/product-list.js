88b2e82368e4273c6a64d3e2cdfde5753c2b5874
Saulius Vikerta <saulius.vikerta@gmail.com>
2017-01-26 07:52:24 +0200

    Fetching data and traversing array. Problem in array

--- app/components/product-list.js
+++ app/components/product-list.js
@@ -10,28 +10,39 @@ import { Col } from 'react-bootstrap';
 
 class ProductList extends Component {
 
+    constructor() {
+        super();
+        this.state = { items: [] };
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/items/data')
+            .then (result => {
+                let result_body = result.text();
+                console.log ('text format: ', result_body);
+                console.log ('response type: ', result.type)
+                return result_body;
+            })
+            .then (text => {
+                return text ? JSON.parse(text) : {}
+            })
+            .then (items_json => {
+                this.setState({items:items_json});
+                console.log ('json format: ',items_json);
+            })
+    }
+
     render() {
         return (
             <Grid className="view-all-products">
                 <Row>
-                    <Col xs={12} md={6} lg={4}>
-                        <ProductCard />
-                    </Col>
-                    <Col xs={12} md={6} lg={4}>
-                        <ProductCard />
-                    </Col>
-                    <Col xs={12} md={6} lg={4}>
-                        <ProductCard />
-                    </Col>
-                    <Col xs={12} md={6} lg={4}>
-                        <ProductCard />
-                    </Col>
-                    <Col xs={12} md={6} lg={4}>
-                        <ProductCard />
-                    </Col>
-                    <Col xs={12} md={6} lg={4}>
-                        <ProductCard />
-                    </Col>
+                    { this.state.items.map (item => { 
+                        return (
+                            <Col xs={12} md={6} lg={4}>
+                                <ProductCard title={item.title} price={item.integerId}/>
+                            </Col>
+                        )
+                    }) }
                 </Row>
             </Grid>
         )
